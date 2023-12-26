3e5dd70d8a344e355f5979a255e63e2dde69a043
Sgrmatkar <sgrmatkar@gmail.com>
2023-12-26 20:04:58 +0530

    assignment 01
    assignment 01

---
+++ helloWorld.js
@@ -0,0 +1,25 @@
+console.log("Hello JS, I am Sagar");
+var num;console.log(num);
+var num="10";
+console.log(num);
+console.log("sagar")
+var myName="sagar";
+console.log(myName);
+console.log("First'Name",myName);
+myName="Sagar Matkar";
+console.log("Full name",myName);
+
+
+console.log("Hello JS , I am Gajanan");
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Gajanan"; // Variable Intialization
+console.log("First Name: ",myName);
+
+var age = "32"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Gajanan Kharat"; // Varibale update
+console.log("Full name: ", myName);
