ff0cce64a7dabdb8f52068d297d6e0639909dc99
Sgrmatkar <sgrmatkar@gmail.com>
2023-12-27 20:26:13 +0530

    Type of datatype, variable basic assign, swap variable
    Type of datatype, variable basic assign, swap variable

---
+++ 02_dataType.js
@@ -0,0 +1,20 @@
+var firstName = "Sagar";
+console.log("Name:",firstName );
+console.log("name:",firstName,",",typeof firstName);
+
+var age = 22;
+var result = typeof age;
+console.log("Type of age: ",result);
+
+var isMarried = false;
+console.log(typeof isMarried);
+var result = typeof isMarried;
+console.log("Type of isMarried: ",result);
+
+var city;
+console.log(city);
+console.log("Type of city: ",typeof city);
+
+var country = null;
+console.log(country);
+console.log('Type of country:', typeof country);
