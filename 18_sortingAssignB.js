cc08554700d6f99e738a554b110fa39acd7825ec
Sgrmatkar <sgrmatkar@gmail.com>
2024-01-29 17:39:14 +0530

    sorting assignment B
    sorting assignment B

---
+++ 18_sortingAssignB.js
@@ -0,0 +1,47 @@
+class Employee {
+    constructor(emp_id, emp_name, emp_dept, emp_salary, emp_company) {
+      this.emp_id = emp_id;
+      this.emp_name = emp_name;
+      this.emp_dept = emp_dept;
+      this.emp_salary = emp_salary;
+      this.emp_company = emp_company;
+    }
+  }
+  const emp_anil = new Employee(22, "Anil", "IT", 50000, "TCS");
+  const emp_radha = new Employee(33, "Radha", "HR", 74000, "Wipro");
+  const emp_rishi = new Employee(55, "Rishi", "Finance", 47000, "TCS");
+  const emp_sonali = new Employee(66, "Sonali", "Finance", 45000, "Infy");
+  const emp_monika = new Employee(77, "Monika", "IT", 40000, "Wipro");
+  const emp_viny = new Employee(88, "Vinayak", "IT", 75000, "TCS");
+  const emp_mahi = new Employee(99, "Mahesh", "HR", 85000, "Infy");
+  const arrayEmployees = [emp_anil, emp_radha, emp_rishi, emp_sonali, emp_monika, emp_viny, emp_mahi];
+  console.log(`-----------------------------------STEP-1-----------------------------------`);
+  console.log(`Employee ID's in descending order->`);
+arrayEmployees.sort((a,b) => {
+    return a.emp_id> b.emp_id ? -1 : 1;
+});
+arrayEmployees.forEach(employee => {
+    console.log(`Employee ID: ${employee.emp_id}, Employee Name: ${employee.emp_name}, Employee Department: ${employee.emp_dept}`);
+});
+console.log(`-----------------------------------STEP-2-----------------------------------`);
+console.log(`Employee Dept in ascending order->`);
+arrayEmployees.sort((a,b) => {
+    if(a.emp_dept < b.emp_dept) return -1;
+    if(a.emp_dept > b.emp_dept) return 1;
+    return 0
+});
+arrayEmployees.forEach(employee => {
+    console.log(`Employee ID: ${employee.emp_id}, Employee Department: ${employee.emp_dept}, Employee Company: ${employee.emp_company}`);
+});
+console.log(`-----------------------------------STEP-3-----------------------------------`);
+
+console.log(`Employee Salary in descending order->`);
+arrayEmployees.sort((a,b) => {
+    if(a.emp_salary < b.emp_salary) return 1;
+    if(a.emp_salary > b.emp_salary) return -1;
+    return 0
+});
+arrayEmployees.forEach(employee => {
+    console.log(`Employee Name: ${employee.emp_name},Employee Sakary: ${employee.emp_salary}, Employee Company: ${employee.emp_company}`);
+});
+console.log(`-----------------------------------END-----------------------------------`);
